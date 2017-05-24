a03b555f5552711152ecf15ca1a3a6105e47f8b9
Kyle Liu <kyleliu97@hotmail.com>
2017-05-24 13:50:00 +0800

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2017-05-24 13:33:51
-# @Author  : Kyle Liu (${email})
-# @Link    : ${link}
-# @Version : $Id$
-
-test file
-how git stage works.
